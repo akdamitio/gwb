24277a96ca3dad0194163840bc64d81f54a31282
akdamitio <80218620+akdamitio@users.noreply.github.com>
2025-06-05 20:27:57 -0700

    Update gwb.py

--- gwb.py
+++ gwb.py
@@ -274,7 +274,7 @@ turf_js = f"""
                         icon: L.divIcon({{
                             className: 'plus-marker',
                             iconSize: [40, 40],
-                            iconAnchor: [11, 32]
+                            iconAnchor: [12, 26]
                         }})
                     }}).addTo({map_var});                    
                 }}
