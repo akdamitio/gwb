e748d3fc3783f4fc012539bafd63f431a6de5f54
akdamitio <80218620+akdamitio@users.noreply.github.com>
2025-06-08 08:04:27 -0700

    Update gwb.py

--- gwb.py
+++ gwb.py
@@ -10,8 +10,7 @@ from streamlit_javascript import st_javascript
 
 # Prepare map
 st.set_page_config(layout="wide")
-with st.container():
-    st.markdown("<h1 style='text-align:center;'>🌍 No Bordle</h1>", unsafe_allow_html=True)
+
 
 js_code = "new Date().toISOString().split('T')[0]"  # e.g., "2025-06-08"
 date_string = st_javascript(js_code=js_code)
@@ -36,7 +35,8 @@ def safe_unicode(s):
 
 
 
-
+with st.container():
+    st.markdown("<h1 style='text-align:center;'>🌍 No Bordle</h1>", unsafe_allow_html=True)
 gdf = load_data()
 
 def get_daily_country(gdf):
