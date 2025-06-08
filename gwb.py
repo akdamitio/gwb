cf8ca07973a0ee970d7407d75cf58976248a59b5
akdamitio <80218620+akdamitio@users.noreply.github.com>
2025-06-08 08:34:48 -0700

    Update gwb.py

--- gwb.py
+++ gwb.py
@@ -42,7 +42,7 @@ gdf = load_data()
 
 def get_daily_country(gdf):
     # Use today's date to get consistent hash
-    today_str = date_string
+    today_str = date_string+37
     time.sleep(2)
     hashed = int(hashlib.sha256(today_str.encode()).hexdigest(), 16)
     idx = hashed % len(gdf)
