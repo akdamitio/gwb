240c329d345b4aebe33fbcaec0a9213ff832aeda
akdamitio <80218620+akdamitio@users.noreply.github.com>
2025-06-09 09:35:25 -0700

    Update gwb.py

--- gwb.py
+++ gwb.py
@@ -47,7 +47,7 @@ selected_name = selected['ADMIN'] if 'ADMIN' in selected else selected['name']
 if(selected['SOVEREIGNT'] != selected_name):
    selected_sov = f" ({selected['SOVEREIGNT']})"
 
-selected_sov = safe_unicode(selected_sov)
+#selected_sov = safe_unicode(selected_sov)
 
 selected_name = safe_unicode(selected_name)
 
