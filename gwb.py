c986486a5b5bc75ea4cf1b2d139ef0bf8822bd14
akdamitio <80218620+akdamitio@users.noreply.github.com>
2025-06-14 12:37:39 -0700

    Update gwb.py

--- gwb.py
+++ gwb.py
@@ -377,6 +377,7 @@ turf_js = f"""
 
 
                     const distanceToBorder = turf.pointToLineDistance(pt, border, {{units: 'miles'}});
+                    console.log(border);
                     if (distanceToBorder < minDistance) {{
                         minDistance = distanceToBorder;
                     }}
