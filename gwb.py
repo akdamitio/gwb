066ffd26870ebd99e09d117e20ce7b06b4737ea9
akdamitio <80218620+akdamitio@users.noreply.github.com>
2025-06-14 12:50:41 -0700

    Update gwb.py

--- gwb.py
+++ gwb.py
@@ -251,7 +251,7 @@ turf_js = f"""
 
         border = turf.polygonToLine(countryGeoJSON);
             if(countryGeoJSON.type === "MultiPolygon"){{
-            console.log(border.coords);
+            console.log(border);
         }};
         
 
