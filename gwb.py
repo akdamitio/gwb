e04880126e517abb0ed6bd0552fedda422f64a36
akdamitio <80218620+akdamitio@users.noreply.github.com>
2025-06-14 13:08:08 -0700

    Update gwb.py

--- gwb.py
+++ gwb.py
@@ -381,6 +381,7 @@ turf_js = f"""
 
                     if (border.type === "FeatureCollection") {{
                         border.features.forEach(f => {{
+                        console.log(f);
                             const dist = turf.distance(pt, f.geometry, {{ units: "miles" }});
                             if (dist < minDistance) {{
                                 minDistance = dist;
