0a813d92c7452397e49f4b90c18d3b50ed39feb5
akdamitio <80218620+akdamitio@users.noreply.github.com>
2025-07-07 14:14:25 -0700

    Update gwb.py

--- gwb.py
+++ gwb.py
@@ -325,7 +325,7 @@ turf_js = f"""
         let holeId = 0;
         var maskPane = {map_var}.createPane("mPane");
 
-        maskPane.style.zIndex = 600;
+        maskPane.style.zIndex = 400;
 
         // Initialize the map
         function initMap() {{
