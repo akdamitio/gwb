87ff4f39d892a2841c9feafc56cd09b567ef72c7
akdamitio <80218620+akdamitio@users.noreply.github.com>
2025-07-07 22:19:39 -0700

    Update gwb.py

--- gwb.py
+++ gwb.py
@@ -333,7 +333,7 @@ turf_js = f"""
             // Add base tile layer
             mapLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{{z}}/{{y}}/{{x}}',{{attribution: 'Tiles © Esri', detectRetina: true, pane: "mPane", keepBuffer: 0, updateWhenZooming: false, updateWhenPanning: false}}).addTo({map_var})        
 
-            const baseLayer = Object.values(map._layers)[0];
+            const baseLayer = Object.values({map_var}._layers)[0];
             // Assuming 'map' is your Leaflet map instance
             {map_var}.on(baseLayer, function () {{
                 document.querySelector('.leaflet-tile-pane').style.visibility = 'hidden';
