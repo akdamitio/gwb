bca5f2b78515b46e41ebc132e0473274fa9aa989
akdamitio <80218620+akdamitio@users.noreply.github.com>
2025-07-10 09:58:46 -0700

    Update gwb.py

--- gwb.py
+++ gwb.py
@@ -231,7 +231,12 @@ turf_js = f"""
         let border;
         border = turf.polygonToLine(countryGeoJSON);
 
-        let totalDistance = 0;
+        if (localStorage.getItem(playedKey + "_totalDistance") != null) {{
+            var totalDistance = Number(localStorage.getItem(playedKey + "_totalDistance"))
+        }} else{{
+            var totalDistance = 0;
+        }};
+        
         function showLosePopup() {{
             const popup = document.createElement('div');
             popup.innerText = `💩 You stink! Average proximity: ${{(totalDistance/6).toFixed(0)}} miles from the border.`;;
